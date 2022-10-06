6cc448010d2bd2906453466a68893900fd48120b
paula di rocco <pau92_02@hotmail.com>
2022-10-05 22:19:34 -0300

    fetch

--- index.js
+++ index.js
@@ -2,71 +2,9 @@ let mensajeCarrito = document.getElementById("mensajeCarrito");
 mensajeCarrito.style.display = "block";
 let carrito = [];
 
-class Producto {
-    constructor (id, nombre, precio, cantidad, imagen){
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.cantidad = cantidad;
-        this.imagen = imagen;
-    }
-}
+//DEFINICION DE FUNCION PARA IR MOSTRANDO EL CARRITO
 
-const productos = [
-    new Producto(1, "notificaciones electronicas", 2900, 20, "/imagenes/notificaciones-electronicas.jpg"),
-    new Producto(2, "manual de contratos", 3400, 17, "/imagenes/manual-de-contratos.jpg"),
-    new Producto(3, "matrimonio y divorcio", 2600, 0, "/imagenes/matrimonio-y-divorcio.jpg"),
-    new Producto(4, "derecho privado empresarial", 4900, 10, "/imagenes/derecho-privado-empresarial.jpg")
-];
-
-let deposito = document.getElementById("deposito");
-
-
-productos.forEach((producto) => {
-    let contenedorProductos = document.createElement("div");
-
-    contenedorProductos.innerHTML = 
-                                    `<img src=${producto.imagen}>
-                                    <h3>${producto.nombre}</h3>
-                                    <p>Precio de lista: ${producto.precio}</p>
-                                    <p>Stock disponible: ${producto.cantidad}</p>
-                                    <button type="submit" id="agregarACarrito-${producto.id}">Agregar al carrito</button>
-                                    <button id="eliminarDeCarrito-${producto.id}">Eliminar del carrito</button>`;
-    deposito.appendChild(contenedorProductos);
-    
-let botonAgregarACarrito = document.getElementById(`agregarACarrito-${producto.id}`);
-botonAgregarACarrito.addEventListener("click", agregar);
-
-//DEFINICION RESPUESTA AGREGAR A CARRITO
-
-function agregar () {
-
-    if (producto.cantidad === 0){
-        alert ("Producto no disponible")
-        document.getElementById(`agregarACarrito-${producto.id}`).setAttribute('disabled','disabled');
-    }else{
-        producto.cantidad = producto.cantidad - 1;
-
-        let id = producto?.id;
-        //console.log(id);
-        //console.log(producto.cantidad);
-        let compra = productos.findIndex(object => {
-            return object.id === id;
-        });
-
-        //console.log(productos[compra]);
-        carrito.push(productos[compra]);
-        localStorage.setItem ("compras", JSON.stringify(carrito));
-        Swal.fire({
-            position: 'center',
-            icon: 'success',
-            title: 'Producto agregado al carrito',
-            showConfirmButton: true,
-            timer: 2500
-        })
-    }
-
-
+function mostrarMensaje () {
     let mensaje = "";
     let productoNro = 0;
     let precioTotal = 0;
@@ -82,18 +20,42 @@ function agregar () {
 
     mensaje+= "Precio total: $"+precioTotal;
     mensajeCarrito.innerText = mensaje;
+    };
 
 
+//DEFINICION RESPUESTA AGREGAR A CARRITO
 
+function agregar (producto) {
+
+    if (producto.cantidad === 0){
+        alert ("Producto no disponible")
+        document.getElementById(`agregarACarrito-${producto.id}`).setAttribute('disabled','disabled');
+    }else{
+        producto.cantidad = producto.cantidad - 1;
+
+        // let id = producto.id;
+        // let compra = productos.findIndex(object => {
+        //     return object.id === id;
+        // });
+
+        // carrito.push(productos[compra]);
+        carrito.push(producto);
+        localStorage.setItem ("compras", JSON.stringify(carrito));
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Producto agregado al carrito',
+            showConfirmButton: true,
+            timer: 2500
+        })
+    }
+
+    mostrarMensaje ();
 };
-
-let botonEliminarDeCarrito = document.getElementById(`eliminarDeCarrito-${producto.id}`);
-botonEliminarDeCarrito.addEventListener("click", eliminar);
-
 
 //DEFINICION RESPUESTA BOTON ELIMINAR
 
-function eliminar(){
+function eliminar(producto){
     Swal.fire({
         title: "Está seguro de eliminar el producto?",
         icon: "warning",
@@ -112,21 +74,7 @@ function eliminar(){
         carrito.splice(eliminarCompra,1);
         localStorage.setItem("compras", JSON.stringify(carrito));
     
-        let mensaje = "";
-        let productoNro = 0;
-        let precioTotal = 0;
-        let misCompras = JSON.parse(localStorage.getItem ("compras"));
-    
-        misCompras.forEach(element => {
-            productoNro++;
-            //productoNro = productoNro + 1; igual a linea anterior
-            mensaje+= "Producto "+productoNro+": "+element.nombre+"\n";
-            precioTotal = precioTotal + element.precio;
-        });
-    
-    
-        mensaje+= "Precio total: $"+precioTotal;
-        mensajeCarrito.innerText = mensaje;
+        mostrarMensaje ();
 
         Swal.fire({
             title: "Borrado!",
@@ -135,41 +83,61 @@ function eliminar(){
         });
         }
     });
-
-
     };
+
+
+//CREACION DE PRODUCTOS
+
+let deposito = document.getElementById("deposito");
+
+fetch("/data.json")
+.then(response => response.json())
+.then(data => {
+    data.forEach(producto => {
+        let contenedorProductos = document.createElement("div");
+
+        contenedorProductos.innerHTML = 
+        `<img src=${producto.imagen}>
+        <h3>${producto.nombre}</h3>
+        <p>Precio de lista: ${producto.precio}</p>
+        <p>Stock disponible: ${producto.cantidad}</p>
+        <button type="submit" id="agregarACarrito-${producto.id}">Agregar al carrito</button>
+        <button id="eliminarDeCarrito-${producto.id}">Eliminar del carrito</button>`;
+        deposito.append(contenedorProductos);
+
+        let botonAgregarACarrito = document.getElementById(`agregarACarrito-${producto.id}`);
+        botonAgregarACarrito.addEventListener("click", () => agregar(producto));
+
+        let botonEliminarDeCarrito = document.getElementById(`eliminarDeCarrito-${producto.id}`);
+        botonEliminarDeCarrito.addEventListener("click", () => eliminar(producto));
+                
+        let mensaje = "";
+        let productoNro = 0;
+        let precioTotal = 0;
+        let misCompras = JSON.parse(localStorage.getItem ("compras"));
+
+        if (misCompras){
+            misCompras.forEach(element => {
+                productoNro++;
+                mensaje+= "Producto "+productoNro+": "+element.nombre+"\n";
+                precioTotal = precioTotal + element.precio;
+            })};
+
+
+        mensaje+= "Precio total: $"+precioTotal;
+        mensajeCarrito.innerText = mensaje;
+
+    });
 });
-
-
-let mensaje = "";
-let productoNro = 0;
-let precioTotal = 0;
-let misCompras = JSON.parse(localStorage.getItem ("compras"));
-
-if (misCompras){
-    misCompras.forEach(element => {
-        productoNro++;
-        //productoNro = productoNro + 1; igual a linea anterior
-        mensaje+= "Producto "+productoNro+": "+element.nombre+"\n";
-        precioTotal = precioTotal + element.precio;
-    })};
-
-
-mensaje+= "Precio total: $"+precioTotal;
-mensajeCarrito.innerText = mensaje;
-
 
 
 
 let botonComprarCarrito = document.getElementById("comprarCarrito");
 botonComprarCarrito.addEventListener("click", comprar);
 
-
 //DEFINICION RESPUESTA BOTON COMPRAR
 function comprar (){
     let mensaje = "";
-
-
     mensaje+= "Gracias por su compra!"
     //Precio total: $"+precioTotal;
     mensajeCarrito.innerText = mensaje;
